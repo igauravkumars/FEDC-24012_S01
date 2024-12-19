9aed251e22931756f486c8816ea56df2f1457e80
Gaurav Kumar Sharma <gauravjabbal@gmail.com>
2024-12-19 21:16:17 +0530

    first commit

---
+++ prob11.js
@@ -0,0 +1,6 @@
+let toDoList = ["Wake up","Brush teeth","Have breakfast"]
+
+toDoList.push("Go for a walk")
+toDoList.pop()
+
+console.log(toDoList)
---
+++ prob12.js
@@ -0,0 +1,5 @@
+let baseMessage = "Hello, [Name]!"
+
+let replace_name = baseMessage.replace("[Name]","Gaurav")
+
+console.log(replace_name)
